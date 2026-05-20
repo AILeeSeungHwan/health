e51d4483fc5ed3af85588105c2909ff0c7611031
AILeeSeungHwan <wizet1923@gmail.com>
2026-05-21 01:50:01 +0900

    Revert "feat: 파비콘 리디자인 — 펄스 하트 (에메랄드 #0E9F6E + 크림)"
    This reverts commit abcad54f20c8a89aa3606427d7954f644e7669bb.

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,11 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0E9F6E" />
-        <meta name="msapplication-TileColor" content="#0E9F6E" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e53935" />
+        <meta name="msapplication-TileColor" content="#e53935" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#0E9F6E" />
+        <meta name="theme-color" content="#e53935" />
       </Head>
       <body><Main /><NextScript /></body>
     </Html>
